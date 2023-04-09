14272dd6f42cdd2de1664737e8742b9f04892396
jannyang <wlghks1725@naver.com>
2023-04-09 21:38:09 +0900

    230409
    Invetory Need to fix

---
+++ 3D-SurvivalGame/Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class InteractionManager : MonoBehaviour
+{
+    public float checkRate = 0.05f;
+    private float lastCheckTime;
+    public float maxCheckDistance;
+    public LayerMask layerMask;
+
+    private GameObject curInteractGameObject;
+    private IInteractable curInteractable;
+
+    public TextMeshProUGUI promptText;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        if(Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            //유저 시점 가운데에서 ray 쏘기
+            Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
+
+            //ray 히트
+            if(Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if(hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+
+            else
+            {
+                curInteractGameObject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = string.Format("<b>[E]</b> {0}", curInteractable.GetInteractPrompt());
+    }
+
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started && curInteractable != null)
+        {
+            curInteractable.OnInteract();
+            curInteractGameObject = null;
+            curInteractable = null;
+            promptText.gameObject.SetActive(false);
+        }
+    }
+}
+public interface IInteractable
+{
+    string GetInteractPrompt();
+    void OnInteract();
+}
+
+
---
+++ 3D-SurvivalGame/Assets/Scripts/Inventory.cs
@@ -0,0 +1,259 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class Inventory : MonoBehaviour
+{
+    public ItemSlotUI[] uiSlots;
+    public ItemSlot[] slots;
+
+    public GameObject inventoryWindow;
+    public Transform dropPosition;
+
+    [Header("Selected Item")]
+    private ItemSlot selectedItem;
+    private int selectedItemIndex;
+    public TextMeshProUGUI selectedItemName;
+    public TextMeshProUGUI selectedItemDescription;
+    public TextMeshProUGUI selectedItemStatNames;
+    public TextMeshProUGUI selectedItemStatValues;
+    public GameObject useButton;
+    public GameObject equipButton;
+    public GameObject unEquipButton;
+    public GameObject dropButton;
+
+    private int curEquipIndex;
+
+    //components
+    private PlayerController controller;
+    private PlayerNeeds needs;
+
+    [Header("Events")]
+    public UnityEvent onOpenInventory;
+    public UnityEvent onCloseInventory;
+
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        instance = this;
+        controller = GetComponent<PlayerController>();
+    }
+
+    private void Start()
+    {
+        inventoryWindow.SetActive(false);
+        slots = new ItemSlot[uiSlots.Length];
+
+        // initialize the slots
+        for(int x =0; x < slots.Length; x++)
+        {
+            slots[x] = new ItemSlot();
+            uiSlots[x].index = x;
+            uiSlots[x].Clear();
+        }
+
+        ClearSelectedItemWindow();
+    }
+
+    public void OnInventoryButton(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started)
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        if(inventoryWindow.activeInHierarchy)
+        {
+            inventoryWindow.SetActive(false);
+            onCloseInventory.Invoke();
+            controller.ToggleCursor(false);
+        }
+
+        else
+        {
+            inventoryWindow.SetActive(true);
+            onOpenInventory.Invoke();
+            ClearSelectedItemWindow();
+            controller.ToggleCursor(true);
+        }
+    }
+
+    public bool IsOpen()
+    {
+        return inventoryWindow.activeInHierarchy;
+    }
+
+    public void AddItem(ItemData item)
+    {
+        //이미 슬롯에 있는 아이템이면 아이템 개수 추가
+        if(item.canStack)
+        {
+            ItemSlot slotToStackTo = GetItemStack(item);
+
+            if(slotToStackTo != null)
+            {
+                slotToStackTo.quantity++;
+                UpdateUI();
+                return;
+            }
+        }
+
+        ItemSlot emptySlot = GetEmptySlot();
+        Debug.Log(emptySlot);
+        //빈 슬롯이 있으면
+        if(emptySlot != null)
+        {
+            emptySlot.item = item;
+            emptySlot.quantity = 1;
+            UpdateUI();
+            return;
+        }
+
+        Debug.Log("Throw Item");
+        //아이템 추가 불가 및 빈 슬롯 없으면 아이템 버리기
+        ThrowItem(item);
+    }
+
+    private void ThrowItem(ItemData item)
+    {
+        Instantiate(item.dropPrefab, dropPosition.position, Quaternion.Euler(Vector3.one * Random.value * 360.0f));
+    }
+
+    private void UpdateUI()
+    {
+        for(int i=0; i<slots.Length; i++)
+        {
+            if (slots[i].item != null)
+                uiSlots[i].Set(slots[i]);
+            else
+                uiSlots[i].Clear();
+        }
+    }
+
+    private ItemSlot GetItemStack(ItemData item)
+    {
+        for(int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i].item == item && slots[i].quantity < item.maxStackAmount)
+                return slots[i];
+        }
+
+        return null;
+    }
+
+    private ItemSlot GetEmptySlot()
+    {
+        Debug.Log("Slot Length : " + slots.Length);
+        for(int i=0; i< slots.Length; i++)
+        {
+            Debug.Log(slots[i].item);
+            if (slots[i].item == null)
+            {
+                Debug.Log(slots[i]);
+                return slots[i];
+            }
+        }
+        return null;
+    }
+
+    public void SelectItem(int index)
+    {
+        if (slots[index].item == null)
+            return;
+
+        selectedItem = slots[index];
+        selectedItemIndex = index;
+
+        selectedItemName.text = selectedItem.item.displayName;
+        selectedItemDescription.text = selectedItem.item.description;
+
+        selectedItemStatNames.text = string.Empty;
+        selectedItemStatValues.text = string.Empty;
+
+        //for(int i =0; i<selectedItem.item.consumables.Length; i++)
+        {
+
+        }
+
+        useButton.SetActive(selectedItem.item.type == ItemType.Consumable);
+        equipButton.SetActive(selectedItem.item.type == ItemType.Equipable && !uiSlots[index].equipped);
+        unEquipButton.SetActive(selectedItem.item.type == ItemType.Equipable && uiSlots[index].equipped);
+        dropButton.SetActive(true);
+    }
+
+    private void ClearSelectedItemWindow()
+    {
+        selectedItem = null;
+        selectedItemName.text = string.Empty;
+        selectedItemDescription.text = string.Empty;
+        selectedItemStatNames.text = string.Empty;
+        selectedItemStatValues.text = string.Empty;
+
+        useButton.SetActive(false);
+        equipButton.SetActive(false);
+        unEquipButton.SetActive(false);
+        dropButton.SetActive(false);
+    }
+
+
+    public void OnUseButton()
+    {
+
+    }
+
+    public void OnEquipButton()
+    {
+
+    }
+
+    private void UnEquip(int index)
+    {
+
+    }
+
+    public void OnUnEquipButton()
+    {
+        
+    }
+
+    public void OnDropButton()
+    {
+        ThrowItem(selectedItem.item);
+        RemoveSelectedItem();
+    }
+
+    private void RemoveSelectedItem()
+    {
+
+    }
+
+    public void RemoveItem(ItemData item)
+    {
+        selectedItem.quantity--;
+
+        if(selectedItem.quantity == 0)
+        {
+            if (uiSlots[selectedItemIndex].equipped == true)
+                UnEquip(selectedItemIndex);
+
+            selectedItem.item = null;
+            ClearSelectedItemWindow();
+        }
+
+        UpdateUI();
+    }
+
+    public bool HasItems(ItemData item, int quantity)
+    {
+        return false;
+    }
+
+}
